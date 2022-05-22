e12ea19866fe3ffbacccc536694c37d29eb8d95e
GUSTAVOPERALTA1 <1719110275@utectulancingo.edu.mx>
2022-05-22 05:04:15 +0000

    v.02

--- code/index.py
+++ code/index.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/docker.html')
+def docker():
+    return render_template('docker.html')
+@app.route('/ubuntu.html')
+def ubuntu():
+    return render_template('ubuntu.html')
 if __name__ == '__main__':
     app.run()
